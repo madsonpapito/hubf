830e2c44a4bb14785e2bdbe171878b1c9e0a169f
mugiwaraventures <tramposon12345@gmail.com>
2026-04-10 17:25:52 +0100

    feat: domains updated to hubfloor.space and twr integration

---
+++ functions/_middleware.js
@@ -0,0 +1,26 @@
+
+export async function onRequest(context) {
+  const response = await context.next();
+  const contentType = response.headers.get("content-type") || "";
+
+  if (!contentType.includes("text/html")) {
+    return response;
+  }
+
+  return new HTMLRewriter()
+    .on("head", {
+      element(element) {
+        element.append(
+          `<script type="text/javascript">
+(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "vsjpirtou2");
+</script>`,
+          { html: true }
+        );
+      }
+    })
+    .transform(response);
+}
